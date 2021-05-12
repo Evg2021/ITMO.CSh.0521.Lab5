1857e503c5eeeaf5e703f71b8499cc07e1183373
Evgeniy Savkov <EvgeniySavkov@itcenterifmo.onmicrosoft.com>
2021-05-12 13:16:24 +0300

    Add project files.

---
+++ FileDetails 2/FileDetails.cs
@@ -0,0 +1,25 @@
+﻿
+using System;                // Console
+using System.IO;             // FileStream, FileReader
+
+class FileDetails
+{
+    static void Main(string[] args)
+    {
+        string fileName = args[0];
+        FileStream stream = new FileStream(fileName, FileMode.Open);
+        StreamReader reader = new StreamReader(stream);
+        int size = (int)stream.Length;
+        char[] contents = new char[size];
+        for (int i = 0; i < size; i++)
+        {
+            contents[i] = (char)reader.Read();
+        }
+        foreach (char ch in contents)
+        {
+            Console.Write(ch);
+
+        }
+
+    }
+}
